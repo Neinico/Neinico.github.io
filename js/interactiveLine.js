cd8a1333801922ababd9154932c3fcfb41d02273
Neinico <32449203+Neinico@users.noreply.github.com>
2019-05-07 22:03:11 -0700

    tes
    test

--- js/interactiveLine.js
+++ js/interactiveLine.js
@@ -1,265 +1,121 @@
-var self = this;
-var myData = "date	New York	San Francisco	Austin\n\
-20111001	63.4	62.7	72.2\n\
-20111002	58.0	59.9	67.7\n\
-20111003	53.3	59.1	69.4\n\
-20111004	55.7	58.8	68.0\n\
-20111005	64.2	58.7	72.4\n\
-20111006	58.8	57.0	77.0\n\
-20111007	57.9	56.7	82.3\n\
-20111008	61.8	56.8	78.9\n\
-20111009	69.3	56.7	68.8\n\
-20111010	71.2	60.1	68.7\n\
-20111011	68.7	61.1	70.3\n\
-20111012	61.8	61.5	75.3\n\
-20111013	63.0	64.3	76.6\n\
-20111014	66.9	67.1	66.6\n\
-20111015	61.7	64.6	68.0\n\
-20111016	61.8	61.6	70.6\n\
-20111017	62.8	61.1	71.1\n\
-20111018	60.8	59.2	70.0\n\
-20111019	62.1	58.9	61.6\n\
-20111020	65.1	57.2	57.4\n\
-20111021	55.6	56.4	64.3\n\
-20111022	54.4	60.7	72.4\n";
+ // Load and munge data, then make the visualization.
+ var fileName = "data/vgsales.csv";
+ //Rank,Name,Platform,Year,Genre,Publisher,NA_Sales,EU_Sales,JP_Sales,Other_Sales,Global_Sales
 
-    /*var margin = {
-        top: 20,
-        right: 80,
-        bottom: 30,
-        left: 50
-      },*/
-        self.width = Math.min(document.getElementById(businessOverview).clientWidth * widthModifier, 300);
-		self.height = document.getElementById(businessOverview).clientHeight;
+ var nutritionFields = ["NA_Sales","EU_Sales", "JP_Sales", "Global_Sales"];
 
-    var parseDate = d3.time.format("%Y%m%d").parse;
+ d3.csv(fileName, function(error, data) {
+     var cerealMap = {};
+     data.forEach(function(d) {
+         var Name = d.Name;
+         cerealMap[Name] = [];
 
-    var x = d3.time.scale()
-      .range([0, width]);
+         // { cerealName: [ bar1Val, bar2Val, ... ] }
+         nutritionFields.forEach(function(field) {
+             cerealMap[Name].push( +d[field] );
+         });
+     });
+     makeVis(cerealMap);
+ });
 
-    var y = d3.scale.linear()
-      .range([height, 0]);
+ var makeVis = function(cerealMap) {
+     // Define dimensions of vis
+     var margin = { top: 30, right: 50, bottom: 30, left: 50 },
+         width  = 550 - margin.left - margin.right,
+         height = 250 - margin.top  - margin.bottom;
 
-    var color = d3.scale.category10();
+     // Make x scale
+     var xScale = d3.scale.ordinal()
+         .domain(nutritionFields)
+         .rangeRoundBands([0, width], 0.1);
 
-    var xAxis = d3.svg.axis()
-      .scale(x)
-      .orient("bottom");
+     // Make y scale, the domain will be defined on bar update
+     var yScale = d3.scale.linear()
+         .range([height, 0]);
 
-    var yAxis = d3.svg.axis()
-      .scale(y)
-      .orient("left");
+     // Create canvas
+     var canvas = d3.select("#taxiPlot")
+       .append("svg")
+         .attr("width",  width  + margin.left + margin.right)
+         .attr("height", height + margin.top  + margin.bottom)
+       .append("g")
+         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    var line = d3.svg.line()
-      .interpolate("basis")
-      .x(function(d) {
-        return x(d.date);
-      })
-      .y(function(d) {
-        return y(d.temperature);
-      });
+     // Make x-axis and add to canvas
+     var xAxis = d3.svg.axis()
+         .scale(xScale)
+         .orient("bottom");
 
-    var svg = d3.select("body").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+     canvas.append("g")
+         .attr("class", "x axis")
+         .attr("transform", "translate(0," + height + ")")
+         .call(xAxis);
 
-    var data = d3.tsv.parse(myData);
+     // Make y-axis and add to canvas
+     var yAxis = d3.svg.axis()
+         .scale(yScale)
+         .orient("left");
 
-    color.domain(d3.keys(data[0]).filter(function(key) {
-      return key !== "date";
-    }));
+     var yAxisHandleForUpdate = canvas.append("g")
+         .attr("class", "y axis")
+         .call(yAxis);
 
-    data.forEach(function(d) {
-      d.date = parseDate(d.date);
-    });
+     yAxisHandleForUpdate.append("text")
+         .attr("transform", "rotate(-90)")
+         .attr("y", 6)
+         .attr("dy", ".71em")
+         .style("text-anchor", "end")
+         .text("Millions");
 
-    var cities = color.domain().map(function(name) {
-      return {
-        name: name,
-        values: data.map(function(d) {
-          return {
-            date: d.date,
-            temperature: +d[name]
-          };
-        })
-      };
-    });
+     var updateBars = function(data) {
+         // First update the y-axis domain to match data
+         yScale.domain( d3.extent(data) );
+         yAxisHandleForUpdate.call(yAxis);
 
-    x.domain(d3.extent(data, function(d) {
-      return d.date;
-    }));
+         var bars = canvas.selectAll(".bar").data(data);
 
-    y.domain([
-      d3.min(cities, function(c) {
-        return d3.min(c.values, function(v) {
-          return v.temperature;
-        });
-      }),
-      d3.max(cities, function(c) {
-        return d3.max(c.values, function(v) {
-          return v.temperature;
-        });
-      })
-    ]);
+         // Add bars for new data
+         bars.enter()
+           .append("rect")
+             .attr("class", "bar")
+             .attr("x", function(d,i) { return xScale( nutritionFields[i] ); })
+             .attr("width", xScale.rangeBand())
+             .attr("y", function(d,i) { return yScale(d); })
+             .attr("height", function(d,i) { return height - yScale(d); });
 
-    var legend = svg.selectAll('g')
-      .data(cities)
-      .enter()
-      .append('g')
-      .attr('class', 'legend');
+         // Update old ones, already have x / width from before
+         bars
+             .transition().duration(250)
+             .attr("y", function(d,i) { return yScale(d); })
+             .attr("height", function(d,i) { return height - yScale(d); });
 
-    legend.append('rect')
-      .attr('x', width - 20)
-      .attr('y', function(d, i) {
-        return i * 20;
-      })
-      .attr('width', 10)
-      .attr('height', 10)
-      .style('fill', function(d) {
-        return color(d.name);
-      });
+         // Remove old ones
+         bars.exit().remove();
+     };
 
-    legend.append('text')
-      .attr('x', width - 8)
-      .attr('y', function(d, i) {
-        return (i * 20) + 9;
-      })
-      .text(function(d) {
-        return d.name;
-      });
+     // Handler for dropdown value change
+     var dropdownChange = function() {
+         var newCereal = d3.select(this).property('value'),
+             newData   = cerealMap[newCereal];
 
-    svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+         updateBars(newData);
+     };
 
-    svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-      .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Temperature (ºF)");
+     // Get names of Games, for dropdown
+     var cereals = Object.keys(cerealMap).sort();
 
-    var city = svg.selectAll(".city")
-      .data(cities)
-      .enter().append("g")
-      .attr("class", "city");
+     var dropdown = d3.select("#taxiPlot")
+         .insert("select", "svg")
+         .on("change", dropdownChange);
 
-    city.append("path")
-      .attr("class", "line")
-      .attr("d", function(d) {
-        return line(d.values);
-      })
-      .style("stroke", function(d) {
-        return color(d.name);
-      });
+     dropdown.selectAll("option")
+         .data(cereals)
+       .enter().append("option")
+         .attr("value", function (d) { return d; })
+         .text(function (d) {
+             return d[0].toUpperCase() + d.slice(1,d.length); // capitalize 1st letter
+         });
 
-    city.append("text")
-      .datum(function(d) {
-        return {
-          name: d.name,
-          value: d.values[d.values.length - 1]
-        };
-      })
-      .attr("transform", function(d) {
-        return "translate(" + x(d.value.date) + "," + y(d.value.temperature) + ")";
-      })
-      .attr("x", 3)
-      .attr("dy", ".35em")
-      .text(function(d) {
-        return d.name;
-      });
-
-    var mouseG = svg.append("g")
-      .attr("class", "mouse-over-effects");
-
-    mouseG.append("path") // this is the black vertical line to follow mouse
-      .attr("class", "mouse-line")
-      .style("stroke", "black")
-      .style("stroke-width", "1px")
-      .style("opacity", "0");
-      
-    var lines = document.getElementsByClassName('line');
-
-    var mousePerLine = mouseG.selectAll('.mouse-per-line')
-      .data(cities)
-      .enter()
-      .append("g")
-      .attr("class", "mouse-per-line");
-
-    mousePerLine.append("circle")
-      .attr("r", 7)
-      .style("stroke", function(d) {
-        return color(d.name);
-      })
-      .style("fill", "none")
-      .style("stroke-width", "1px")
-      .style("opacity", "0");
-
-    mousePerLine.append("text")
-      .attr("transform", "translate(10,3)");
-
-    mouseG.append('svg:rect') // append a rect to catch mouse movements on canvas
-      .attr('width', width) // can't catch mouse events on a g element
-      .attr('height', height)
-      .attr('fill', 'none')
-      .attr('pointer-events', 'all')
-      .on('mouseout', function() { // on mouse out hide line, circles and text
-        d3.select(".mouse-line")
-          .style("opacity", "0");
-        d3.selectAll(".mouse-per-line circle")
-          .style("opacity", "0");
-        d3.selectAll(".mouse-per-line text")
-          .style("opacity", "0");
-      })
-      .on('mouseover', function() { // on mouse in show line, circles and text
-        d3.select(".mouse-line")
-          .style("opacity", "1");
-        d3.selectAll(".mouse-per-line circle")
-          .style("opacity", "1");
-        d3.selectAll(".mouse-per-line text")
-          .style("opacity", "1");
-      })
-      .on('mousemove', function() { // mouse moving over canvas
-        var mouse = d3.mouse(this);
-        d3.select(".mouse-line")
-          .attr("d", function() {
-            var d = "M" + mouse[0] + "," + height;
-            d += " " + mouse[0] + "," + 0;
-            return d;
-          });
-
-        d3.selectAll(".mouse-per-line")
-          .attr("transform", function(d, i) {
-            console.log(width/mouse[0])
-            var xDate = x.invert(mouse[0]),
-                bisect = d3.bisector(function(d) { return d.date; }).right;
-                idx = bisect(d.values, xDate);
-            
-            var beginning = 0,
-                end = lines[i].getTotalLength(),
-                target = null;
-
-            while (true){
-              target = Math.floor((beginning + end) / 2);
-              pos = lines[i].getPointAtLength(target);
-              if ((target === end || target === beginning) && pos.x !== mouse[0]) {
-                  break;
-              }
-              if (pos.x > mouse[0])      end = target;
-              else if (pos.x < mouse[0]) beginning = target;
-              else break; //position found
-            }
-            
-            d3.select(this).select('text')
-              .text(y.invert(pos.y).toFixed(2));
-              
-            return "translate(" + mouse[0] + "," + pos.y +")";
-          });
-      });
-      
+     var initialData = cerealMap[ cereals[0] ];
+     updateBars(initialData);
+ }
